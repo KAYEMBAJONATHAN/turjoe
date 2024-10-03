002a3db898e7ff0b1a6382e8bf77e43975efcd83
KayembaJonathan <kayembejonathan5@gmail.com>
2024-10-03 19:50:29 +0200

    Add the img

--- apps/web/app/homePage.tsx
+++ apps/web/app/homePage.tsx
@@ -1,9 +1,11 @@
 'use client';
 import React from 'react';
+import welcome from '../public/welcome.jpg'
 
 export default function HomePage() {
   return (
-    <div className="ui-flex iui-tems-center ui-justify-center ui-min-h-screen ui-bg-gray-100 ui-text-black ui-font-cursive">
+    <div className="ui-flex ui-items-center ui-justify-center ui-min-h-screen ui-bg-gray-50 ui-text-black ui-font-cursive ui-flex-col">
+      <img src="/welcome.jpg" alt="welcome" className="ui-w-80 ui-h-auto ui-mb-8" />
       <h1 className="ui-text-4xl font-bold">Welcome to My App</h1>
     </div>
   );
